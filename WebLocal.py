8f99ac683eda6269a4a37729ab76c370f35be5f2
Artem Ananev <ananev.artem272005@gmail.com>
2021-02-23 11:15:31 +0300

    5com

--- WebLocal.py
+++ WebLocal.py
@@ -21,37 +21,46 @@ def promotion():
 
 @app.route('/image_mars')
 def mars_img():
-    return '<h1 class="h1_image_mars">Жди нас, Марс!<h1><img src="/static/img/mars.jpg" alt="!!!ТУТ ФОТО!!!" />'
+    return '<h1>Жди нас, Марс!<h1>' \
+           '<img src="/static/img/mars.jpg" alt="!!!ТУТ ФОТО!!!" />' \
+           '<p>Вот она какая, красная планета.</p>'
 
 
 @app.route('/promotion_image')
 def promotion_image():
-    return '''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <link rel="stylesheet" href="/static/css/style.css" type="text/css">
-                    <title>Привет, Яндекс!</title>
-                  </head>
-                  <body>
-                    <h1 class="h1_image_mars">Жди нас, Марс!<h1>
-                    <img src="/static/img/mars.jpg" alt="!!!ТУТ ФОТО!!!" />
-                    <div class="block1">
-                        Человечество вырастает из детства.
-                    </div>
-                    <div class="block2">
-                        Человечеству мала одна планета.
-                    </div>
-                    <div class="block3">
-                        Мы сделаем обитаемыми безжизненные пока планеты.
-                    </div>
-                    <div class="block4">
-                        И начнем с Марса!
-                    </div>
-                    <div class="block5">
-                        Присоединяйся!
-                    </div>
-                  </body>
-                </html>'''
+    color = ['dark', 'success', 'info', 'danger', 'warning', 'primary']
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <link rel="stylesheet" href="/static/css/style.css" type="text/css">
+                        <title>Реклама с картинкой</title>
+                      </head>
+                      <body>
+                        <h1 class="h1_image_mars">Жди нас, Марс!<h1>
+                        <img src="/static/img/mars.jpg" alt="!!!ТУТ ФОТО!!!" />
+                        <div class="alert alert-{random.choice(color)} role="alert">
+                            Человечество вырастает из детства.
+                        </div>
+                        <div class="alert alert-{random.choice(color)} role="alert">
+                            Человечеству мала одна планета.
+                        </div>
+                        <div class="alert alert-{random.choice(color)} role="alert">
+                            Мы сделаем обитаемыми безжизненные пока планеты.
+                        </div>
+                        <div class="alert alert-{random.choice(color)} role="alert">
+                            И начнем с Марса!
+                        </div>
+                        <div class="alert alert-{random.choice(color)} role="alert">
+                            Присоединяйся!
+                        </div>
+                      </body>
+                    </html>'''
 
 
 @app.route('/astronaut_selection')
